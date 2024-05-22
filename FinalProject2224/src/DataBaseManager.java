d4eb784f5494073f1ad0a1d7b1ea3def15c6a271
Arfushs <36006828+Arfushs@users.noreply.github.com>
2024-05-22 16:41:19 +0300

    Add/Delete

--- FinalProject2224/src/DataBaseManager.java
+++ FinalProject2224/src/DataBaseManager.java
@@ -13,7 +13,7 @@ public class DataBaseManager {
         this.databasePass = databasePass;
     }
 
-    public void AddNewVisit(String username,String countryName,String cityName, String season, String feature,String comment,int rating)
+    public void AddNewVisit(String username,String countryName,String cityName, int yearVisited,String season, String feature,String comment,int rating)
     {
         try {
             Connection conn = DriverManager.getConnection(databaseURL,databaseUser,databasePass);
@@ -21,17 +21,18 @@ public class DataBaseManager {
             Statement statement = conn.createStatement();
             String visitIDQuery = "select visitID from visits order by visitID desc limit 1";
             ResultSet resultSet = statement.executeQuery(visitIDQuery);
-            int visitID =  resultSet.next() ? resultSet.getInt("userID") + 1: 0;
+            int visitID =  resultSet.next() ? resultSet.getInt("visitID") + 1: 0;
 
-            PreparedStatement preparedStatement = conn.prepareStatement("insert into visits(visitID,username,country_name,city_name,season,feature,comment,rating) values(?,?,?,?,?,?,?,?)");
+            PreparedStatement preparedStatement = conn.prepareStatement("insert into visits(visitID,username,country_name,city_name,year_visited,season,feature,comment,rating) values(?,?,?,?,?,?,?,?,?)");
             preparedStatement.setInt(1, visitID);
             preparedStatement.setString(2, username);
             preparedStatement.setString(3, countryName);
             preparedStatement.setString(4, cityName);
-            preparedStatement.setString(5, season);
-            preparedStatement.setString(6, feature);
-            preparedStatement.setString(7, comment);
-            preparedStatement.setInt(8, rating);
+            preparedStatement.setInt(5, yearVisited);
+            preparedStatement.setString(6, season);
+            preparedStatement.setString(7, feature);
+            preparedStatement.setString(8, comment);
+            preparedStatement.setInt(9, rating);
             preparedStatement.executeUpdate();
 
             System.out.println("The new entry has been successfully added to the database !");
--- FinalProject2224/src/Demo.java
+++ FinalProject2224/src/Demo.java
@@ -1,27 +1,66 @@
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.JTableHeader;
-import javax.swing.table.TableColumnModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
 
 public class Demo extends JFrame{
-    static String databaseURL = "jdbc:mysql://localhost:3306/project";
+    static String databaseURL = "jdbc:mysql://127.0.0.1:3306/project";
     static String databaseUser = "root";
-    static String databasePass = "12345678";
+    static String databasePass = "C856500n.";
     static DataBaseManager dataBaseManager;
 
     private JPanel mainPanel;
-    private JButton button1;
+    private JButton tableButton;
     private JTable dataTable;
+    private JButton addLocationButton;
+    private JTextField countryTextField;
+    private JLabel countryLabel;
+    private JTextField cityTextField;
+    private JTextField yearTextField;
+    private JTextField seasonTextField;
+    private JTextField bestFeatureTextField;
+    private JTextField commentTextField;
+    private JTextField ratingTextField;
+    private JButton deleteVisitButton;
+    private JTextField deleteWVITextField;
 
 
     public Demo() {
-        button1.addActionListener(new ActionListener() {
+        tableButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 ShowTable();
+            }
+        });
+        addLocationButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Boolean b = cityTextField.getText().length() > 0 && countryTextField.getText().length() >0 &&
+                        seasonTextField.getText().length() > 0 && bestFeatureTextField.getText().length() >0 &&
+                        commentTextField.getText().length() > 0 && ratingTextField.getText().length() >0 &&
+                        yearTextField.getText().length() > 0;
+
+                if (b) {
+                    String Username = "", Country = countryTextField.getText(), City = cityTextField.getText(), Year = yearTextField.getText(),
+                            Season = seasonTextField.getText(), BestFeature = bestFeatureTextField.getText(), Comment = commentTextField.getText(),
+                    Rating = ratingTextField.getText();
+                    dataBaseManager.AddNewVisit(Username,Country,City,Integer.valueOf(Year),Season,BestFeature,Comment,Integer.valueOf(Rating));
+                    ShowTable();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Please enter all the fields..");
+                }
+
+            }
+        });
+        deleteVisitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int id = Integer.parseInt(deleteWVITextField.getText());
+                dataBaseManager.DeleteVisit(id);
+                JOptionPane.showMessageDialog(null, "The visit has been deleted. (Visit ID: " +id+")");
+                ShowTable();
+
             }
         });
     }
@@ -49,18 +88,19 @@ public class Demo extends JFrame{
                     model.removeRow(0);
 
             String username,country_name,city_name,season,feature,comment;
-            int visitID,rate;
+            int visitID,rate,year_visited;
             while(resultSet.next())
             {
                 visitID = resultSet.getInt(1);
-                rate = resultSet.getInt(8);
+                rate = resultSet.getInt(9);
                 username = resultSet.getString(2);
                 country_name = resultSet.getString(3);
                 city_name = resultSet.getString(4);
-                season = resultSet.getString(5);
-                feature = resultSet.getString(6);
-                comment = resultSet.getString(7);
-                String row[] = {String.valueOf(visitID),username,country_name,city_name,season,feature,comment, String.valueOf(rate)};
+                year_visited = resultSet.getInt(5);
+                season = resultSet.getString(6);
+                feature = resultSet.getString(7);
+                comment = resultSet.getString(8);
+                String row[] = {String.valueOf(visitID),username,country_name,city_name,String.valueOf(year_visited),season,feature,comment, String.valueOf(rate)};
                 model.addRow(row);
             }
             statement.close();
@@ -72,7 +112,6 @@ public class Demo extends JFrame{
             e.printStackTrace();
         }
 
-
     }
 
     public static void main(String[] args) {
@@ -80,6 +119,7 @@ public class Demo extends JFrame{
         dataBaseManager = new DataBaseManager(databaseURL,databaseUser,databasePass);
 
         Demo demo = new Demo();
+        demo.setTitle("Project");
         demo.setContentPane(demo.mainPanel);
         demo.setSize(600,500);
         demo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
